9af633a40ab888bbb4e52542ee53ded97228f0f3
Devank18 <67155685+Devank18@users.noreply.github.com>
2020-10-11 12:49:38 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/MatterJSBoilerPlate-master/ground.js
@@ -0,0 +1,33 @@
+class ground
+{
+	constructor(x,y,width,height)
+	{
+		var options={
+			isStatic:true
+			}
+			this.body=Bodies.rectangle(x, y, width, height , options);
+			this.width=width
+			this.height=height
+			this.x=x;
+		this.y=y;
+
+
+        World.add(world, this.body);
+
+	}
+	display()
+	{
+
+			var groundPos=this.body.position;
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			fill(255,255,0)
+			rect(0,0,this.width, this.height);
+			pop()
+			
+	}
+
+}
